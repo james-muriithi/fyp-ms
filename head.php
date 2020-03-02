0a4763d5bf8eaeaf09622c98a5bc9147e62813e0
james-muriithi <muriithijames556@gmail.com>
2020-03-02 23:11:48 +0300

    added poster image

--- head.php
+++ head.php
@@ -12,14 +12,14 @@
     <meta name="keywords" content="fyp,fypms, final year project">
     <meta property="og:site_name" content="google.com">
     <meta property="og:title" content="Final Year Project Management System" />
-    <meta property="og:description" content="A final year project management system for pwani university" />
-    <!-- <meta property="og:image:url" itemprop="image" content="https://james-muriithi.github.io/bus/images/logo-400.png">
-    <meta property="og:image" content="https://james-muriithi.github.io/bus/images/logo-400.png" />
-    <meta property="og:image:url" content="https://james-muriithi.github.io/bus/images/logo-400.png" />
-    <meta property="og:image:secure_url" content="https://james-muriithi.github.io/bus/images/logo-400.png" />
+    <meta property="og:description" content="A final year project management system for pwani university made by james muriithi" />
+    <meta property="og:image:url" itemprop="image" content="https://james-muriithi.github.io/fyp-ms/assets/images/logo-lg.png">
+    <meta property="og:image" content="https://james-muriithi.github.io/fyp-ms/assets/images/logo-lg.png" />
+    <meta property="og:image:url" content="https://james-muriithi.github.io/fyp-ms/assets/images/logo-lg.png" />
+    <meta property="og:image:secure_url" content="https://james-muriithi.github.io/fyp-ms/assets/images/logo-lg.png" />
     <meta property="og:image:type" content="image/png" />
     <meta property="og:image:width" content="400" />
-    <meta property="og:image:height" content="400" /> -->
+    <meta property="og:image:height" content="400" />
     <meta property="og:locale" content="en_GB" />
     <meta property="og:type" content="website" />
     <!-- App favicon -->
