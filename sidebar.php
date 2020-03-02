74e2e603a2934bb7f15582ab6a99db4c9380e8aa
james-muriithi <muriithijames556@gmail.com>
2020-03-02 22:04:11 +0300

    added siedbar.php

---
+++ sidebar.php
@@ -0,0 +1,207 @@
+<!-- ========== Left Sidebar Start ========== -->
+        <div class="vertical-menu">
+            <div data-simplebar class="h-100">
+                <!--- Sidemenu -->
+                <div id="sidebar-menu">
+                    <!-- Left Menu Start -->
+                    <ul class="metismenu list-unstyled" id="side-menu">
+                        <li class="menu-title">Main</li>
+                        <li>
+                            <a href="index.html" class="waves-effect">
+                                <span>Dashboard</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="calendar.html" class=" waves-effect">
+                                <i class="ti-calendar"></i>
+                                <span>Calendar</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                <i class="ti-email"></i>
+                                <span>Email</span>
+                            </a>
+                            <ul class="sub-menu" aria-expanded="false">
+                                <li><a href="email-inbox.html">Inbox</a></li>
+                                <li><a href="email-read.html">Email Read</a></li>
+                                <li><a href="email-compose.html">Email Compose</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-title">Components</li>
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                <i class="ti-package"></i>
+                                <span>UI Elements</span>
+                            </a>
+                            <ul class="sub-menu" aria-expanded="false">
+                                <li><a href="ui-alerts.html">Alerts</a></li>
+                                <li><a href="ui-buttons.html">Buttons</a></li>
+                                <li><a href="ui-cards.html">Cards</a></li>
+                                <li><a href="ui-carousel.html">Carousel</a></li>
+                                <li><a href="ui-dropdowns.html">Dropdowns</a></li>
+                                <li><a href="ui-grid.html">Grid</a></li>
+                                <li><a href="ui-images.html">Images</a></li>
+                                <li><a href="ui-lightbox.html">Lightbox</a></li>
+                                <li><a href="ui-modals.html">Modals</a></li>
+                                <li><a href="ui-rangeslider.html">Range Slider</a></li>
+                                <li><a href="ui-session-timeout.html">Session Timeout</a></li>
+                                <li><a href="ui-progressbars.html">Progress Bars</a></li>
+                                <li><a href="ui-sweet-alert.html">Sweet-Alert</a></li>
+                                <li><a href="ui-tabs-accordions.html">Tabs &amp; Accordions</a></li>
+                                <li><a href="ui-typography.html">Typography</a></li>
+                                <li><a href="ui-video.html">Video</a></li>
+                                <li><a href="ui-general.html">General</a></li>
+                                <li><a href="ui-colors.html">Colors</a></li>
+                                <li><a href="ui-rating.html">Rating</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript: void(0);" class="waves-effect">
+                                <i class="ti-receipt"></i>
+                                <span class="badge badge-pill badge-success float-right">6</span>
+                                <span>Forms</span>
+                            </a>
+                            <ul class="sub-menu" aria-expanded="false">
+                                <li><a href="form-elements.html">Form Elements</a></li>
+                                <li><a href="form-validation.html">Form Validation</a></li>
+                                <li><a href="form-advanced.html">Form Advanced</a></li>
+                                <li><a href="form-editors.html">Form Editors</a></li>
+                                <li><a href="form-uploads.html">Form File Upload</a></li>
+                                <li><a href="form-xeditable.html">Form Xeditable</a></li>
+                                <li><a href="form-repeater.html">Form Repeater</a></li>
+                                <li><a href="form-wizard.html">Form Wizard</a></li>
+                                <li><a href="form-mask.html">Form Mask</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                <i class="ti-pie-chart"></i>
+                                <span>Charts</span>
+                            </a>
+                            <ul class="sub-menu" aria-expanded="false">
+                                <li><a href="charts-morris.html">Morris Chart</a></li>
+                                <li><a href="charts-chartist.html">Chartist Chart</a></li>
+                                <li><a href="charts-chartjs.html">Chartjs Chart</a></li>
+                                <li><a href="charts-flot.html">Flot Chart</a></li>
+                                <li><a href="charts-knob.html">Jquery Knob Chart</a></li>
+                                <li><a href="charts-sparkline.html">Sparkline Chart</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                <i class="ti-view-grid"></i>
+                                <span>Tables</span>
+                            </a>
+                            <ul class="sub-menu" aria-expanded="false">
+                                <li><a href="tables-basic.html">Basic Tables</a></li>
+                                <li><a href="tables-datatable.html">Data Table</a></li>
+                                <li><a href="tables-responsive.html">Responsive Table</a></li>
+                                <li><a href="tables-editable.html">Editable Table</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                <i class="ti-face-smile"></i>
+                                <span>Icons</span>
+                            </a>
+                            <ul class="sub-menu" aria-expanded="false">
+                                <li><a href="icons-material.html">Material Design</a></li>
+                                <li><a href="icons-fontawesome.html">Font Awesome</a></li>
+                                <li><a href="icons-ion.html">Ion Icons</a></li>
+                                <li><a href="icons-themify.html">Themify Icons</a></li>
+                                <li><a href="icons-dripicons.html">Dripicons</a></li>
+                                <li><a href="icons-typicons.html">Typicons Icons</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript: void(0);" class="waves-effect">
+                                <i class="ti-location-pin"></i>
+                                <span class="badge badge-pill badge-danger float-right">2</span>
+                                <span>Maps</span>
+                            </a>
+                            <ul class="sub-menu" aria-expanded="false">
+                                <li><a href="maps-google.html"> Google Map</a></li>
+                                <li><a href="maps-vector.html"> Vector Map</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-title">Extras</li>
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                <i class="ti-layout"></i>
+                                <span> Layouts </span>
+                            </a>
+                            <ul class="sub-menu" aria-expanded="false">
+                                <li><a href="layouts-horizontal.html">Horizontal</a></li>
+                                <li><a href="layouts-compact-sidebar.html">Compact Sidebar</a></li>
+                                <li><a href="layouts-icon-sidebar.html">Icon Sidebar</a></li>
+                                <li><a href="layouts-boxed.html">Boxed Layout</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                <i class="ti-archive"></i>
+                                <span> Authentication </span>
+                            </a>
+                            <ul class="sub-menu" aria-expanded="false">
+                                <li><a href="pages-login.html">Login 1</a></li>
+                                <li><a href="pages-login-2.html">Login 2</a></li>
+                                <li><a href="pages-register.html">Register 1</a></li>
+                                <li><a href="pages-register-2.html">Register 2</a></li>
+                                <li><a href="pages-recoverpw.html">Recover Password 1</a></li>
+                                <li><a href="pages-recoverpw-2.html">Recover Password 2</a></li>
+                                <li><a href="pages-lock-screen.html">Lock Screen 1</a></li>
+                                <li><a href="pages-lock-screen-2.html">Lock Screen 2</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                <i class="ti-support"></i>
+                                <span> Extra Pages </span>
+                            </a>
+                            <ul class="sub-menu" aria-expanded="false">
+                                <li><a href="pages-timeline.html">Timeline</a></li>
+                                <li><a href="pages-invoice.html">Invoice</a></li>
+                                <li><a href="pages-directory.html">Directory</a></li>
+                                <li><a href="pages-blank.html">Blank Page</a></li>
+                                <li><a href="pages-404.html">Error 404</a></li>
+                                <li><a href="pages-500.html">Error 500</a></li>
+                                <li><a href="pages-pricing.html">Pricing</a></li>
+                                <li><a href="pages-gallery.html">Gallery</a></li>
+                                <li><a href="pages-maintenance.html">Maintenance</a></li>
+                                <li><a href="pages-comingsoon.html">Coming Soon</a></li>
+                                <li><a href="pages-faq.html">FAQs</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                <i class="ti-bookmark-alt"></i>
+                                <span> Email Templates </span>
+                            </a>
+                            <ul class="sub-menu" aria-expanded="false">
+                                <li><a href="email-template-basic.html">Basic Action Email</a></li>
+                                <li><a href="email-template-Alert.html">Alert Email</a></li>
+                                <li><a href="email-template-Billing.html">Billing Email</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                <i class="ti-more"></i>
+                                <span>Multi Level</span>
+                            </a>
+                            <ul class="sub-menu" aria-expanded="true">
+                                <li><a href="javascript: void(0);">Level 1.1</a></li>
+                                <li><a href="javascript: void(0);" class="has-arrow">Level 1.2</a>
+                                    <ul class="sub-menu" aria-expanded="true">
+                                        <li><a href="javascript: void(0);">Level 2.1</a></li>
+                                        <li><a href="javascript: void(0);">Level 2.2</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <!-- Sidebar -->
+            </div>
+        </div>
+        <!-- Left Sidebar End -->
