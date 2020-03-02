f0bfacbc0552910d4f50c26657710d724686c02c
james-muriithi <muriithijames556@gmail.com>
2020-03-02 22:02:27 +0300

    added head.php

---
+++ head.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>FYPMS | Final Year Project Management System</title>
+    <!-- meta tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#25274d">
+    <meta name="description" content="A final year project management system for pwani university">
+    <meta name="author" content="James Muriitih">
+    <meta name="keywords" content="fyp,fypms, final year project">
+    <meta property="og:site_name" content="google.com">
+    <meta property="og:title" content="Final Year Project Management System" />
+    <meta property="og:description" content="A final year project management system for pwani university" />
+    <!-- <meta property="og:image:url" itemprop="image" content="https://james-muriithi.github.io/bus/images/logo-400.png">
+    <meta property="og:image" content="https://james-muriithi.github.io/bus/images/logo-400.png" />
+    <meta property="og:image:url" content="https://james-muriithi.github.io/bus/images/logo-400.png" />
+    <meta property="og:image:secure_url" content="https://james-muriithi.github.io/bus/images/logo-400.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="400" />
+    <meta property="og:image:height" content="400" /> -->
+    <meta property="og:locale" content="en_GB" />
+    <meta property="og:type" content="website" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    <link href="assets/libs/chartist/chartist.min.css" rel="stylesheet">
+    <!-- Bootstrap Css -->
+    <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+    <!-- aniamte css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <!-- util css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/util.css">
+    <!-- style -->
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
