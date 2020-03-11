67589d331c28bbdc5a31fa436b4852b7d187e8e7
james-muriithi <muriithijames556@gmail.com>
2020-03-11 10:39:08 +0300

    new api functions

--- api/sendMail.php
+++ api/sendMail.php
@@ -15,14 +15,19 @@ if (isset($data['message'],$data['to'], $data['subject'])) {
     $message = $data['message'];
     $to = $data['to'];
     $subject = $data['subject'];
-    sendMail($to,$message,$subject);
+    if (isset($data['from']) && !empty($data['from'])){
+        echo json_encode(sendMail($to,$message,$subject,$data['from']));
+        die();
+    }
+    echo json_encode(sendMail($to,$message,$subject));
+    die();
 }
 
-function sendMail($to, $message, $subject, $cc='')
+function sendMail($to, $message, $subject, $from = 'James Muriithi', $cc='')
 {
     // To send HTML mail, the Content-type header must be set
-    $headers  = "From: James Muriithi < support@theschemaqhigh.co.ke >\r\n";
-    $headers .= "X-Sender: James Muriithi < support@theschemaqhigh.co.ke >\r\n";
+    $headers  = 'From: ' .$from." < support@theschemaqhigh.co.ke >\r\n";
+    $headers .= 'X-Sender: ' .$from." < support@theschemaqhigh.co.ke >\r\n";
     $headers .= 'X-Mailer: PHP/' . PHP_VERSION ."\r\n";
     $headers .= "X-Priority: 1\r\n"; // Urgent message!
     $headers .= "Return-Path: support@theschemaqhigh.co.ke\r\n"; // Return path for errors
@@ -32,5 +37,12 @@ function sendMail($to, $message, $subject, $cc='')
         $headers .= 'Cc: '.$cc . "\r\n";
     }
 
-    return @mail($to, $subject, $message, $headers);
+    return array('sent' => @mail($to, $subject, $message, $headers),
+        'details'=> array(
+            'to'=>$to,
+            'from' => $from,
+            'message' => $message,
+            'fromEmail'=> 'support@theschemaqhigh.co.ke'
+        )
+    );
 }
--- api/sendMessage.php
+++ api/sendMessage.php
@@ -51,26 +51,35 @@ function sendMsg($phone, $msg){
     return true;
 }
 
-function sendMail($to, $msg, $subject){
-
+function sendMail($to, $msg, $subject, $from=''){
+//    required headers
     $stkheader = array('Content-Type:application/json','Cache-Control:no-cache');
+//    url to post the data
     $url = 'https://theschemaqhigh.co.ke/api/sendMail.php';
 
-    # initiating the transaction
+    # initiating curl
     $curl = curl_init();
+//    set curl url
     curl_setopt($curl, CURLOPT_URL, $url);
+//    set cur headers
     curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
 
+//    data to be posted to the url
     $curl_post_data = array(
         'message' => $msg,
         'to' => $to,
         'subject' =>  $subject,
+        'from'=>$from
     );
 
+//json encode the data
     $data_string = json_encode($curl_post_data);
+//    expect to return value from url
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+//    set method to POST
     curl_setopt($curl, CURLOPT_POST, true);
+//    set the data to post
     curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
-    $curl_response = curl_exec($curl);
-    return json_encode($curl_response);
+//    print the response
+    print_r(curl_exec($curl));
 }
